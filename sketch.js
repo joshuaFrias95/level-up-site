3fa0111cd5f001845c47aee0ee3b5c81509435ab
Joshua F. <metatron1495@gmail.com>
2025-08-20 20:05:18 -0600

    Bug: No se muestra sketch || se busca error

--- sketch.js
+++ sketch.js
@@ -5,13 +5,14 @@ let fireworkTimer = 0;
 let textTimer = 0;
 
 function preload() {
-  pixelFont = loadFont('fonts/PressStart2P-Regular.ttf');
+  // pixelFont = loadFont('fonts/PressStart2P-Regular.ttf');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
-  textFont(pixelFont);
+  // textFont(pixelFont);
+  textFont('monospace');
   textSize(24);
   textAlign(CENTER, CENTER);
   noStroke();
